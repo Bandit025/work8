3d001b08afddc11e38ef1fdbd85e81c28af59ce2
BND34 <banthitnanthyothawuthi@gmail.com>
2024-04-24 14:02:28 +0700

    Initial commit

---
+++ connect.php
@@ -0,0 +1,3 @@
+<?php 
+    $conn=mysqli_connect("localhost","root","","work8") or die("เกิดข้อผิดพลาดเกิดขึ้น");
+?>
---
+++ sql.sql
@@ -0,0 +1,4 @@
+CREATE TABLE prefix(
+id_prefix int(5) NOT NULL,
+name_prefix varchar(255)NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
